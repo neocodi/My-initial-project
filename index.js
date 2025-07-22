fbb12d420e619edc85db76fc15657e4d62b2d7fd
neocodi <rlaflecheleblanc2025@gmail.com>
2025-07-21 21:06:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function main(){
- var menu = document.createElement("div");
+ var menu = document.createElement("div")
  with(menu){
-   id="rollover_frame";
-   innerHTML="rollingTest";
+   id="rollover_frame"
+   innerHTML="rollingTest"
    with(style){
-     width="100px";
-     height="50px";
-     backgroundColor="red";
- } }
+     width="100px"
+     height="50px"
+     backgroundColor="red"
+ }; };
 
  /*menu.id = "menu_frame";
 menu.style.backgroundColor = "red";
 menu.style.width = "100px";
 menu.style.height = "25px";*/
 
-document.getElementById("top_wrap").appendChild("menu");
-}
+document.getElementById("top_wrap").appendChild("menu")
+};
