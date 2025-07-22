997569d59c914817cfd370f367f7518f9d270b3a
neocodi <rlaflecheleblanc2025@gmail.com>
2025-07-21 20:58:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 3function main(){
  var menu = document.createElement("div");
  menu.id = "menu_frame";
+menu.style.backgroundColor = "red";
+menu.style.width = "100px";
+menu.style.height = "25px";
+document.getElementById("top_wrap").appendChild(menu);
 }
