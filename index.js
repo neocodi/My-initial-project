9c5b062e971f040862af09d3b92f6840ea889721
neocodi <rlaflecheleblanc2025@gmail.com>
2025-08-16 10:22:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-ql function main(){
+function main(){
  var menu = document.createElement("div")
- 
    menu.id = "menu_frame";
    menu.style.backgroundColor = "red";
    menu.style.width = "100px";
    menu.style.height = "25px";
 
-document.getElementById("top_wrap").appendChild(menu)
+	document.getElementById("top_wrap").appendChild(menu)
 
 	for(var i = 0; i < 5; i++){
 		var opt = document.createElement("div");
 		opt.id="option"+i
 		opt.innerHTML=namer[i]
-}		
+	}		
 
 };
-var namer = {
-			menu = {
-							fr = 
-window.onload=main;
+
