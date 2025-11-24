b6f1d294ac80411ebd17809ec789a7609a46c20e
neocodi <rlaflecheleblanc2025@gmail.com>
2025-11-24 15:28:20 -0500

    Remove main function and menu creation logic

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-function main(){
- var menu = document.createElement("div")
-   menu.id = "menu_frame";
-   menu.style.backgroundColor = "red";
-   menu.style.width = "100px";
-   menu.style.height = "25px";
-
-	document.getElementById("top_wrap").appendChild(menu)
-
-	for(var i = 0; i < 5; i++){
-		var opt = document.createElement("div");
-		opt.id="option"+i
-		opt.innerHTML=namer[i]
-	}		
-
-};
 
